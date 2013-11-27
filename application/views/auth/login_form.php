83aec7227d5172ba548f4cdf0afd80e023983a7e
shadowbeam <xvb09137@baird-12.cis.strath.ac.uk>
2013-11-27 16:54:37 +0000

    captcha theming

--- application/views/auth/login_form.php
+++ application/views/auth/login_form.php
@@ -69,40 +69,42 @@ $captcha = array(
 
 						<div class="form-group">
 
-							<div class="pull-left" id="recaptcha_image"></div>
+							<div class="div-center">
+								<div class="pull-left" id="recaptcha_image"></div>
 
-							<div class="btn-group-vertica pull-left">
-								<a class="btn btn-default" alt="Get another CAPTCHA" href="javascript:Recaptcha.reload()"><i class="glyphicon glyphicon-refresh"></i></a>
-								<div class="recaptcha_only_if_image"><a alt ="Get an audio CAPTCHA" class="btn btn-default" href="javascript:Recaptcha.switch_type('audio')"><i class="glyphicon glyphicon-volume-up"></i></a></div>
-								<div class="recaptcha_only_if_audio"><a alt="Get an image CAPTCHA" class="btn btn-default"href="javascript:Recaptcha.switch_type('image')"><i class="glyphicon glyphicon-picture"></i></a></div>
+								<div class="btn-group-vertica pull-left">
+									<a class="btn btn-default" alt="Get another CAPTCHA" href="javascript:Recaptcha.reload()"><i class="glyphicon glyphicon-refresh"></i></a>
+									<div class="recaptcha_only_if_image"><a alt ="Get an audio CAPTCHA" class="btn btn-default" href="javascript:Recaptcha.switch_type('audio')"><i class="glyphicon glyphicon-volume-up"></i></a></div>
+									<div class="recaptcha_only_if_audio"><a alt="Get an image CAPTCHA" class="btn btn-default"href="javascript:Recaptcha.switch_type('image')"><i class="glyphicon glyphicon-picture"></i></a></div>
+								</div>
+							</div>
+							<?php echo form_error('recaptcha_response_field'); ?>
+
+							<div class="form-group">
+								<input type="text" class="form-control recaptcha_only_if_image" placeholder = "Enter the words above" id="recaptcha_response_field" name="recaptcha_response_field" />
+								<input type="text" class="form-control recaptcha_only_if_audio" placeholder = "Enter the numbers you hear" id="recaptcha_response_field" name="recaptcha_response_field" />
+								<?php echo $recaptcha_html; ?>
 							</div>
 						</div>
-						<?php echo form_error('recaptcha_response_field'); ?>
-
-						
-						<input type="text" class="form-control recaptcha_only_if_image" placeholder = "Enter the words above" id="recaptcha_response_field" name="recaptcha_response_field" />
-						<input type="text" class="form-control recaptcha_only_if_audio" placeholder = "Enter the numbers you hear" id="recaptcha_response_field" name="recaptcha_response_field" />
-						<?php echo $recaptcha_html; ?>
-					</tr>
-				</div>
-				<?php }?>
-
-				<div class="form-group">
-					<?php echo anchor('/auth/forgot_password/', 'Forgot password?', 'class="pull-left"'); ?>
-
-					<?php echo form_submit('submit', 'Login', 'class="btn btn-primary btn-default pull-right"'); ?>
-
-					<div id ="remember-me" class="checkbox text-left pull-right clearfix">
-						<label>
-							<?php echo form_checkbox($remember); ?>  Remember Me		
-						</label>
 					</div>
-				</div>
+					<?php }?>
 
-			</form>
+					<div class="form-group">
+						<?php echo anchor('/auth/forgot_password/', 'Forgot password?', 'class="pull-left"'); ?>
 
+						<?php echo form_submit('submit', 'Login', 'class="btn btn-primary btn-default pull-right"'); ?>
+
+						<div id ="remember-me" class="checkbox text-left pull-right clearfix">
+							<label>
+								<?php echo form_checkbox($remember); ?>  Remember Me		
+							</label>
+						</div>
+					</div>
+
+				</form>
+
+			</div>
 		</div>
 	</div>
-</div>
 </div>
 
