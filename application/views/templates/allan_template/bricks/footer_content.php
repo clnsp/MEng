1fdc1614d42080f57d127d50aed0ee8a4bc50ed0
shadowbeam <xvb09137@baird-08.cis.strath.ac.uk>
2014-02-13 11:18:57 +0000

    removed unnused footer stuff

--- application/views/templates/allan_template/bricks/footer_content.php
+++ application/views/templates/allan_template/bricks/footer_content.php
@@ -1,52 +1,52 @@
   <hr/>
 
   <div class="row">
-    <div class="col-md-3 col-md-offset-1" style="text-align:center">Social Media</div>
+  <!--   <div class="col-md-3 col-md-offset-1" style="text-align:center">Social Media</div>
     <div class="col-md-4" style="text-align:center">Contact Us</div>
-    <div class="col-md-4" style="text-align:center">Useful Links</div>
+    <div class="col-md-4" style="text-align:center">Useful Links</div> -->
   </div>
 
   <div class="row">
-    	<div class="col-md-3 col-md-offset-1" style="text-align:center">
-<?php foreach($media as $d): ?>
-<?php $url = $d['twitter_id'] ?>
-<?php $url = prep_url($url); ?>
+   <div class="col-md-3 col-md-offset-1" style="text-align:center">
+    <?php foreach($media as $d): ?>
+    <?php $url = $d['twitter_id'] ?>
+    <?php $url = prep_url($url); ?>
     <?php echo "<a href='".$url."'>" ?> <span class="fa-stack fa-lg">
-      <i class="fa fa-circle fa-stack-2x fa-inverse"></i>
-      <i class="fa fa-twitter fa-stack-1x"></i>
-    </span><?php echo "</a>" ?>
+    <i class="fa fa-circle fa-stack-2x fa-inverse"></i>
+    <i class="fa fa-twitter fa-stack-1x"></i>
+  </span><?php echo "</a>" ?>
 <?php endforeach ?>
 <?php foreach($media as $d): ?>
-<?php $url = $d['facebook_id'] ?>
-<?php $url = prep_url($url); ?>
-    <?php echo "<a href='".$url."'>" ?> <span class="fa-stack fa-lg">
-      <i class="fa fa-circle fa-stack-2x fa-inverse"></i>
-      <i class="fa fa-facebook fa-stack-1x"></i>
-    </span><?php echo "</a>" ?>
+  <?php $url = $d['facebook_id'] ?>
+  <?php $url = prep_url($url); ?>
+  <?php echo "<a href='".$url."'>" ?> <span class="fa-stack fa-lg">
+  <i class="fa fa-circle fa-stack-2x fa-inverse"></i>
+  <i class="fa fa-facebook fa-stack-1x"></i>
+</span><?php echo "</a>" ?>
 <?php endforeach ?>
 <?php foreach($media as $d): ?>
-<?php $url = $d['googleplus_id'] ?>
-<?php $url = prep_url($url); ?>
-    <?php echo "<a href='".$url."'>" ?> <span class="fa-stack fa-lg">
-      <i class="fa fa-circle fa-stack-2x fa-inverse"></i>
-      <i class="fa fa-google-plus fa-stack-1x"></i>
-    </span><?php echo "</a>" ?>
+  <?php $url = $d['googleplus_id'] ?>
+  <?php $url = prep_url($url); ?>
+  <?php echo "<a href='".$url."'>" ?> <span class="fa-stack fa-lg">
+  <i class="fa fa-circle fa-stack-2x fa-inverse"></i>
+  <i class="fa fa-google-plus fa-stack-1x"></i>
+</span><?php echo "</a>" ?>
 <?php endforeach ?>
-	</div>
-    	<div class="col-md-4" style="text-align:center">
+</div>
+<div class="col-md-4" style="text-align:center">
 	<?php foreach($contacts as $d): ?>
-		<?php echo "<a href='mailto:".$d['email']."'>Contact</a>" ?>
-	<?php endforeach; ?>
-	</div>
-    	<div class="col-md-4" style="text-align:center">
+  <?php echo "<a href='mailto:".$d['email']."'>Contact</a>" ?>
+<?php endforeach; ?>
+</div>
+<div class="col-md-4" style="text-align:center">
 	<?php foreach($links as $d): ?>
-		<?php $url = $d['link'] ?>
-		<?php $url = prep_url($url); ?>
-		<?php echo "<a href='".$url."'>".$d['description']."</a><br>" ?>
-	<?php endforeach; ?>
-	</div>
-   </div>
+  <?php $url = $d['link'] ?>
+  <?php $url = prep_url($url); ?>
+  <?php echo "<a href='".$url."'>".$d['description']."</a><br>" ?>
+<?php endforeach; ?>
+</div>
+</div>
 
-  <div  class="center-block">
+<div  class="center-block">
 
-  </div>
+</div>
