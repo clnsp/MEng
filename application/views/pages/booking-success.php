4368311cf8ad5e6bd01d4bedfd5f342523846bd5
shadowbeam <xvb09137@open-special-04.cis.strath.ac.uk>
2014-03-25 18:28:58 +0000

    fixed the my bookings link and added new make another booking button

--- application/views/pages/booking-success.php
+++ application/views/pages/booking-success.php
@@ -1,19 +1,19 @@
 <div class="page-header text-center">
-  <div class="well well-lg">
+	<div class="well well-lg">
 
-    <h1>Your Booking was Successful!<br> <i class="glyphicon glyphicon-ok"></i> </h1>
+		<h1>Your Booking was Successful!<br> <i class="glyphicon glyphicon-ok"></i> </h1>
 
-    <h1><small>You will be attending <b><?php echo $classinfo['class_type'];?></b>
+		<h1><small>You will be attending <b><?php echo $classinfo['class_type'];?></b>
 
 
-     <br>at <b><?php $d = new DateTime($classinfo['class_start_date']); echo $d->format("H:i")?> </b> on 
-     <b><?php echo $d->format("jS F Y")?></b> in <b><?php echo $classinfo['room'];?></b> 
-   </small></h1>
+			<br>at <b><?php $d = new DateTime($classinfo['class_start_date']); echo $d->format("H:i")?> </b> on 
+			<b><?php echo $d->format("jS F Y")?></b> in <b><?php echo $classinfo['room'];?></b> 
+		</small></h1>
+		<div class="button-group">
+			<a class="btn btn-primary" href="<?php echo site_url()?>/booking/">Make Another Booking</a>
+			<a class="btn btn-primary" href="<?php echo site_url()?>/booking/mybookings">View My Bookings</a>
+		</div>
 
-   <a href="<?php echo base_url()?>index.php/mybookings"><button type="button" class="btn btn-primary" >
-    View My Bookings  
-    
-  </button></a>
-</div>
+	</div>
 
 </div>
--- application/views/pages/booking-wait.php
+++ application/views/pages/booking-wait.php
@@ -1,26 +1,26 @@
 <div class="page-header text-center">
-<div class="well well-lg">
-  
-  <h1>Fully Booked
-<br> <i class="glyphicon glyphicon-time"></i>
-  </h1>
+  <div class="well well-lg">
+    
+    <h1>Fully Booked
+      <br> <i class="glyphicon glyphicon-time"></i>
+    </h1>
+    
+    <h1><small><p><b><?php echo $class_type;?></b> starting at <b><?php $d = new DateTime($class_start_date); echo $d->format("H:i")?> </b> on 
+     <b><?php echo $d->format("jS F Y")?></b> in <b><?php echo $room;?></b> is fully booked. </p>
+     
+     <p>You may add yourself to the waiting list to be notified if a space becomes available. When spaces become available, they are assigned on a first come first served basis.</p>  
+   </small></h1>
+   
+   <?php 
+   echo form_open("/booking/joinWaiting");
+   echo form_hidden(array('class_id' =>  $class_id));
+   echo form_submit('submit', 'Join Waiting List', "class='btn btn-primary'");
+   echo form_close();
+   ?>
+ </td>
  
-  <h1><small><p><b><?php echo $class_type;?></b> starting at <b><?php $d = new DateTime($class_start_date); echo $d->format("H:i")?> </b> on 
-  	<b><?php echo $d->format("jS F Y")?></b> in <b><?php echo $room;?></b> is fully booked. </p>
-  	
-  	<p>You may add yourself to the waiting list to be notified if a space becomes available. When spaces become available, they are assigned on a first come first served basis.</p>  
-</small></h1>
-  
- 		<?php 
-	 		echo form_open("/booking/joinWaiting");
-	 		echo form_hidden(array('class_id' =>  $class_id));
-	 		echo form_submit('submit', 'Join Waiting List', "class='btn btn-primary'");
-		 	echo form_close();
- 		?>
- 	</td>
  
  
- 
-  </div>
-  
+</div>
+
 </div>
