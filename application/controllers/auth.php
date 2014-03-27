62f7bd5a25c9e8c8c74ce835b159f2a7f359b937
Peter Davies CSM2009 <gmb09157@open-special-03.cis.strath.ac.uk>
2014-03-27 16:09:45 +0000

    fixed email

--- application/controllers/auth.php
+++ application/controllers/auth.php
@@ -757,14 +757,14 @@ $data['memberTypes'] = $this->Members->getAllMemberTypes();
 //		$this->email->reply_to($this->config->item('webmaster_email', 'tank_auth'), $this->config->item('website_name', 'tank_auth'));
 		$this->email->from($fromEmail, 'Email Test');
 		$this->email->to($email);
-		$this->email->subject('Emails Tests');
+		$this->email->subject('Registration Confirmation');
 //		$this->email->subject(sprintf($this->lang->line('auth_subject_'.$type), $this->config->item('website_name', 'tank_auth')));
-		$this->email->message('Testing the email class.'); 
+		$this->email->message('This is to confirm that your account registraion has been successful congratulations ya rocket.'); 
 //		$this->email->message($this->load->view('email/'.$type.'-html', $data, TRUE));
 //		$this->email->set_alt_message($this->load->view('email/'.$type.'-txt', $data, TRUE));
 		$this->email->send();
-		echo("SEND1");
-		echo $this->email->print_debugger();
+
+//		echo $this->email->print_debugger(); // Useful email debugger
 	}
 
 	/**
