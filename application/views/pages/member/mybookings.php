822feba1cc1e19d07992ac2517b09cbc6cdb3135
Peter Davies CSM2009 <gmb09157@open-special-03.cis.strath.ac.uk>
2014-03-27 09:39:09 +0000

    changed bookings page waiting list

--- application/views/pages/member/mybookings.php
+++ application/views/pages/member/mybookings.php
@@ -69,6 +69,74 @@
 
 			</div>
 
+						<?php if(count($waiting) != 0){ ?>
+
+<div class="panel panel-default">
+			<div class="panel-heading">
+				<h3 class="panel-title">Current Waiting List</h3>
+			</div>
+			<div class="panel-body">	
+
+				<div class="booking-panel">		
+
+					<table id='current-waiting-table'  class="table footable table-bordered table-striped table-hover" data-page-navigation=".current-pagination">
+
+
+						<thead>
+							<tr>
+								<th>Class</th>
+								<th data-hide="phone">Room</th>
+								<th data-type="date">Date</th>
+								<th data-sort-initial="true" data-type="time">Start</th>
+								<th data-hide="phone" data-type="time">End</th>
+								<th data-sort-ignore="true" data-hide="phone">Cancel</th>		
+							</tr>
+						</thead>
+
+						<tbody>
+							<?php foreach($waiting as $d): 
+							$form = array('class' => 'form','role' => 'form',);
+							$startDate = new DateTime($d['start']); 			
+							$endDate = new DateTime($d['end']);
+
+							$hidden = array(
+								'class_booking_id' =>$d['class_id'] ,
+								'member_id' => $d['member_id']
+								); ?>
+
+
+								<tr booking_id="<?php echo $d['member_id'] ?>">
+
+									<td class="class_type"><?php echo $d['title'] ?></td>
+									<td class="room"><?php echo $d['room'] ?></td>
+									<td data-value="<?php echo $startDate->getTimestamp();?>"class="start_date"><?php echo $startDate->format('jS F Y') ?></td>
+									<td data-value="<?php echo $startDate->getTimestamp();?>" class="start_time"><?php echo $startDate->format('h.i A') ?></td>
+									<td data-value="<?php echo $endDate->getTimestamp();?>" class="end_time"><?php echo $endDate->format('h.i A') ?></td>
+									<td class="cancel">
+
+										<?php 
+										echo form_open(site_url(). "/booking/cancelWaiting", $form, $hidden);
+										echo form_submit('cancelWaiting', 'Cancel Waiting', "class='cancelwaiting btn btn-sm btn-danger'");	
+										echo form_close(); 
+										?>
+
+									</td>
+								</tr>
+							<?php endforeach; ?>
+
+							
+							</tbody>
+						</table>
+					</div>	
+					
+					<div class="hide-if-no-paging current-pagination pull-right pagination">
+						<ul class="pagination"></ul>
+					</div>
+				</div>
+
+			</div>
+								<?php }?>
+
 			<div class="panel panel-default">
 				<div class="panel-heading">
 					<h3 class="panel-title">Past Bookings</h3>
@@ -145,72 +213,6 @@
 			</div>
 
 		</div>
-
-		<div class="panel panel-default">
-			<div class="panel-heading">
-				<h3 class="panel-title">Current Waiting</h3>
-			</div>
-			<div class="panel-body">	
-
-				<div class="booking-panel">		
-
-					<table id='current-waiting-table'  class="table footable table-bordered table-striped table-hover" data-page-navigation=".current-pagination">
-
-						<?php if(count($waiting) != 0){ ?>
-						<thead>
-							<tr>
-								<th>Class</th>
-								<th data-hide="phone">Room</th>
-								<th data-type="date">Date</th>
-								<th data-sort-initial="true" data-type="time">Start</th>
-								<th data-hide="phone" data-type="time">End</th>
-								<th data-sort-ignore="true" data-hide="phone">Cancel</th>		
-							</tr>
-						</thead>
-
-						<tbody>
-							<?php foreach($waiting as $d): 
-							$form = array('class' => 'form','role' => 'form',);
-							$startDate = new DateTime($d['start']); 			
-							$endDate = new DateTime($d['end']);
-
-							$hidden = array(
-								'class_booking_id' =>$d['class_id'] ,
-								'member_id' => $d['member_id']
-								); ?>
-
-
-								<tr booking_id="<?php echo $d['member_id'] ?>">
-
-									<td class="class_type"><?php echo $d['title'] ?></td>
-									<td class="room"><?php echo $d['room'] ?></td>
-									<td data-value="<?php echo $startDate->getTimestamp();?>"class="start_date"><?php echo $startDate->format('jS F Y') ?></td>
-									<td data-value="<?php echo $startDate->getTimestamp();?>" class="start_time"><?php echo $startDate->format('h.i A') ?></td>
-									<td data-value="<?php echo $endDate->getTimestamp();?>" class="end_time"><?php echo $endDate->format('h.i A') ?></td>
-									<td class="cancel">
-
-										<?php 
-										echo form_open(site_url(). "/booking/cancelWaiting", $form, $hidden);
-										echo form_submit('cancelWaiting', 'Cancel Waiting', "class='cancelwaiting btn btn-sm btn-danger'");	
-										echo form_close(); 
-										?>
-
-									</td>
-								</tr>
-							<?php endforeach; ?>
-
-							<?php }else{
-								echo("<td class='nofound' colspan='6'><h2>No classes found</h2></td>");}?>
-							</tbody>
-						</table>
-					</div>	
-					
-					<div class="hide-if-no-paging current-pagination pull-right pagination">
-						<ul class="pagination"></ul>
-					</div>
-				</div>
-
-			</div>
 
 		</div>
 
