48a36a9baf7af019e84d3c2fa441e16307d7901c
Colin Espie CSM2009 <xvb09151@open-special-06.cis.strath.ac.uk>
2014-03-28 15:31:50 +0000

    Caladner fix

--- application/controllers/calendar.php
+++ application/controllers/calendar.php
@@ -220,9 +220,6 @@ class Calendar extends CI_Controller{
       $params = getQueryStringParams();
 
       if((isset($params['start']) && isset($params['end']))){
-	$this->classes->removeSportClass($class_booking_id); //if class is a sport need to remove the class as well
-				$this->load->helper('temp');
-				notifyWaiting($class_booking_id, "T
         $s = gmdate("Y-m-d H:i:s", $params['start']);
         $e = gmdate("Y-m-d H:i:s", $params['end']);
 
