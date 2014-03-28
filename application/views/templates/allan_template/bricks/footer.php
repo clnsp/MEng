ca7dff6d69a40a18ba94407890e12eb730b542d0
shadowbeam <xvb09137@open-special-04.cis.strath.ac.uk>
2014-03-28 15:14:09 +0000

    fix for duplicate adding

--- application/views/templates/allan_template/bricks/footer.php
+++ application/views/templates/allan_template/bricks/footer.php
@@ -43,7 +43,6 @@ if(!$this->tank_auth->is_admin()){ ?>
      <script src="<?php echo base_url();?>assets/js/jquery-ui-autocomplete.custom.min.js"></script>
      <script src="<?php echo base_url();?>assets/cal/fullcalendar.min.js"></script>
      <script type="text/javascript" src="<?php echo base_url();?>assets/js/booking-calendar.js"></script>
-     <script type="text/javascript" src="<?php echo base_url();?>assets/js/manage.js"></script>
 
      <?php //}?>
      <!-- Admin Manage Scripts -->
